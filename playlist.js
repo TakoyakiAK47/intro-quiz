a6c251461a916c26f3e78b2c4dee41cd6fc905e5
TakoyakiAK47 <71084775+TakoyakiAK47@users.noreply.github.com>
2026-05-29 00:41:31 +0900

    Add new OST entries to playlist

--- playlist.js
+++ playlist.js
@@ -309,6 +309,8 @@ const playlist = [
   { title: "Otomegokoro", videoId: "C8MpBkjjPTs", composer: "Mitsukiyo", context: "OST 340\nメモロビ:リオ(臨戦)", imageUrl: "https://i.ytimg.com/vi/C8MpBkjjPTs/hqdefault.jpg" },
   { title: "OST 341", videoId: "mP_dEztTzyI", composer: "EmoCosine", context: "OST 341\nメモロビ:ヒマリ(臨戦)", quiz: false, imageUrl: "https://i.ytimg.com/vi/mP_dEztTzyI/hqdefault.jpg" },
   { title: "OST 342", videoId: "QsruZz8Xjb4", composer: "Unknown", context: "OST 342\nメモロビ:トキ(臨戦)", quiz: false, imageUrl: "https://i.ytimg.com/vi/QsruZz8Xjb4/hqdefault.jpg" },
+  { title: "OST 352", videoId: "nl0uujg5v2E", composer: "Unknown", context: "OST 352", quiz: false, imageUrl: "https://i.ytimg.com/vi/nl0uujg5v2E/hqdefault.jpg" },
+  { title: "Train Showdown", videoId: "xFQ-g_cJDAE", composer: "KARUT", context: "OST -", imageUrl: "https://i.ytimg.com/vi/xFQ-g_cJDAE/hqdefault.jpg" },
 ];
 
 const characterSongPlaylist = [
